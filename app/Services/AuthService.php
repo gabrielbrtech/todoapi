9ff34fcfdb1278ddc704bb083b9f5dbb934b7bf4
gabrielbrtech <gabrielcamurca@brtech.dev>
2023-04-02 10:38:36 -0300

    Create register endpoint

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,6 +3,9 @@
 namespace App\Services;
 
 use App\Exceptions\LoginInvalidException;
+use App\Exceptions\UserHasBeenTakenException;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class  AuthService {
     public function login(string $email, string $password) {
@@ -21,5 +24,24 @@ class  AuthService {
             ];
         }
 
+    }
+
+    public function register(string $first_name, string $last_name, string $email, string $password) {
+        $user = User::where('email', $email)->exists();
+        if (!empty($user)){
+            throw new UserHasBeenTakenException;
+        }
+
+        $userPassword = bcrypt($password ?? Str::random(10));
+
+        $user = User::create([
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $email,
+            'password' => $userPassword,
+            'confirmation_token' => Str::random(60),
+        ]);
+
+        return $user;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::get('test', function() {
 
 Route::prefix('v1')->group(function() {
     Route::post('login',[AuthController::class, 'login']);
+    Route::post('register',[AuthController::class, 'register']);
 });
