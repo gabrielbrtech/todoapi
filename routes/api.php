2f0578651a232b273d6c5fc7bb139231269c1924
gabrielbrtech <gabrielcamurca@brtech.dev>
2023-04-01 22:13:37 -0300

    Create login service + route pt1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,6 @@ Route::get('test', function() {
     return response()->json(['msg' => 'Funcionando!']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+    Route::post('login',[AuthController::class, 'login']);
 });
